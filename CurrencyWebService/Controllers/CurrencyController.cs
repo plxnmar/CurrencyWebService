c4eeac2226a68dbff6f2eb84a396fe0b57a0aa59
Mira0Kit <kitomura@bk.ru>
2023-01-18 10:35:08 +0300

    добавлен контроллер

---
+++ CurrencyWebService/Controllers/CurrencyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyWebService.Controllers
+{
+	[ApiController]
+	public class CurrencyController : ControllerBase
+	{
+		[HttpGet]
+		[Route("currencies")]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		[HttpGet("{id}")]
+		[Route("currency/{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+	}
+}
